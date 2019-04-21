9b4b8e8cd5af00c487e9ebf6214aad473151feb8
kouhei ueno <k.cola119@gmail.com>
2019-04-22 01:53:12 +0900

    aa

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
     include ActionController::HttpAuthentication::Token::ControllerMethods
-    # before_action :authenticate, except: [:new]
+    before_action :authenticate, except: [:new]
     def new
         @user = User.new(user_params)
         return response_bad_request(@user.errors.full_messages.first) unless @user.valid?
@@ -38,7 +38,7 @@ class Api::V1::UsersController < ApplicationController
 
     private
         def user_params
-            params.require(:user).permit(:user_id, :password, :nickname, :comment, :token)
+            params.permit(:user_id, :password, :nickname, :comment, :token)
         end
 
         def authenticate
