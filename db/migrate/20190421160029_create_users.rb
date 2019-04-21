db886189a2f6dad7ab1e5b11e46c25de5962709e
kouhei ueno <k.cola119@gmail.com>
2019-04-22 01:12:33 +0900

    a

--- db/migrate/20190421160029_create_users.rb
+++ db/migrate/20190421160029_create_users.rb
@@ -3,8 +3,12 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :user_id
       t.string :password
+      t.string :nickname
+      t.string :comment
+      t.string :token
 
       t.timestamps
     end
+    add_index :users, :token, unique: true
   end
 end
